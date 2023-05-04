fba4ae2805cc7604119549cdc389c5ddc910a015
GabrieleFerreira <gabriele.ferreira@ufvjm.edu.br>
2023-05-04 11:50:32 -0300

    layout login

--- estudos-nextjs13/src/app/auth/login/page.tsx
+++ estudos-nextjs13/src/app/auth/login/page.tsx
@@ -1,10 +1,9 @@
 import { SigInput } from "./components/SigInput";
 
-export default function Login () {
-
+export default function Login() {
   return (
     <>
-      <section className="bg-red-500 dark:bg-gray-900">
+      <section className=" dark:bg-gray-900">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 ">
           <div className="w-full p-2 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -22,17 +21,16 @@ export default function Login () {
                   placeholder="••••••••"
                 />
 
-                <div className="flex  justify-center px-4">
-                  <button className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500 bg-red-400 flex-grow rounded-md">
+                <div className="flex  justify-center ">
+                  <button className="text-sm font-medium mt-8 sm:p-2 text-blue-600 hover:underline dark:text-blue-500 bg-red-400 flex-grow rounded-md">
                     Entrar
-                  </button>     
-            </div>
-            </div>
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </section>
-     
     </>
-  )
+  );
 }
